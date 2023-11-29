4a12f1e5de33110087336b8021a301fa6018c7e0
Matt <matt@ithinkanalytics.com>
2023-11-29 10:58:01 +0000

    This is a recommit as correct a couple of late bugs found in side cases

--- milestone_5.py
+++ milestone_5.py
@@ -7,17 +7,17 @@ class hangman():
         self.word = random.choice(word_list)
         self.num_letters = len(self.word)
         self.word_guessed = list('_' * len(self.word))
-        #self.word_guessed = list(self.word)
         self.list_of_guesses = []
+
 
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word.lower():
             print(f"Good guess! {guess} is in the word.")
-            self.num_letters -= 1
             for i, letter in enumerate(self.word):
-                if guess.lower() == letter.lower():
-                    self.word_guessed[i] = guess.lower()
+                if letter.lower() == guess.lower():
+                    self.num_letters -= 1
+                    self.word_guessed[i] = letter
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
@@ -28,7 +28,7 @@ class hangman():
             guess = input(f'Hangman letter guess, please enter a single letter ...')
             if len(guess) != 1 or guess.isalpha() != True:
                print(f"Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in [self.list_of_guesses]:
+            elif guess in self.list_of_guesses:
                 print(f"You already tried that letter!")
             else:
                 self.check_guess(guess)
@@ -50,9 +50,7 @@ def play_game(word_list):
         else:
             print(f'There is an error!')
             break
-
-#hang = hangman(["Orange", "Apple", "Banana", "Pear", "Raspberry"],5)
-#hang.ask_for_input()
+    print(f'The word is {game.word}')
 
 play_game(["Orange", "Apple", "Banana", "Pear", "Raspberry"])
 
